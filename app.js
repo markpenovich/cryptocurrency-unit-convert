0947c22236df133f1293cb1086fad28f04df40ce
Mark Penovich <mpenovich1@gmail.com>
2018-01-13 18:13:05 -0600

    Added more units

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 var Units = require('./index.js')
 
-const convertedXRP = Units.convertXRP('1', 'drop', 'xrp') // '0.000000000000000001'
+const convertedValue = Units.convertBTC('1', 'btc', 'bit') // '0.000000000000000001'
 
-// Convert 1.5 xrp to drop
-const convertedXRP1 = Units.convertXRP('1.5', 'xrp', 'drop') // '1500000000000000000'
-
-// Convert 1 photon to ltc
-const convertedLTC = Units.convertLTC('1', 'photon', 'ltc') // '0.000000000000000001'
-
-// Convert 1.5 ltc to photon
-const convertedLTC1 = Units.convertLTC('1.5', 'ltc', 'photon') // '1500000000000000000'
-
-console.log(convertedXRP);
-console.log(convertedXRP1);
-console.log(convertedLTC);
-console.log(convertedLTC1);
+console.log(convertedValue);
